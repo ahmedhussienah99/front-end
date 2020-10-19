d7eda21299ae72cb258d79532672e193309a2e5e
ahmedhussienah99 <61842483+ahmedhussienah99@users.noreply.github.com>
2020-10-19 15:29:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    
+
+
+  
+
+ mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+$("#myBtn").on("click", function(){
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+});
+
+  
+//  $("#menu").click(function(){
+//  $(".showheader").css({"display": "block"});
+//  $(".header").css({"display": "none"});
+//});
+ 
+
+
+});
